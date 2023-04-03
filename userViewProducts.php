8e4a9664b9589e978ed8d66b2b554d2712847fb8
amina khurram 1 <127178584+aminakhurram@users.noreply.github.com>
2023-04-03 16:34:21 -0500

    Add files via upload

--- userViewProducts.php
+++ userViewProducts.php
@@ -46,6 +46,7 @@ if(isset($_POST['add_to_cart'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/Css.css"/>
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -57,7 +58,7 @@ if(isset($_POST['add_to_cart'])){
 <script type="text/javascript" src="script/deleteRecords.js"></script>
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="style.css">
+
 </head>
 <body>
     
